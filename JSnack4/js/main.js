feeca29372797499068eb8df1da5d2b6b150cf80
DanieleMarino70 <danielaotitan@gmail.com>
2023-01-30 23:37:26 +0100

    add: snack5

--- JSnack4/js/main.js
+++ JSnack4/js/main.js
@@ -1,6 +1,6 @@
 // Snack4 (Bonus)
 // Fai inserire un numero all'utente. Poi continua a chiedere numeri finché la serie è crescente (l'ultimo numero inserito deve essere maggiore del precedente). Alla fine stampali tutti.
-debugger;
+
 let userNumber = parseInt(prompt("Inserisci numero: "));
 let counter = 0;
 const growingArray = [];
--- JSnack5/js/main.js
+++ JSnack5/js/main.js
@@ -2,3 +2,38 @@
 // Genera un numero a caso compreso tra 1-100. Chiedi all’utente un numero fino a quando non indovina quello generato casualmente. 
 // Ogni volta che l'utente inserisce un numero, digli se è maggiore o minore di quello generato. 
 // Alla fine stampa il numero di tentativi che ci sono voluti per indovinare il numero generato casualmente.
+
+
+
+const randomGeneratedNumber = Math.floor(Math.random() * 100 + 1);
+//console.log(randomGeneratedNumber);
+
+let userNumber = 0;
+let counter = 0;
+let check = false;
+debugger
+
+// CON GOOGLE CHROME SI INCASTRA, HO DOVUTO METTERE L'ALERT
+
+do{
+    userNumber = parseInt(prompt("Scegli un numero da 1 a 100"));
+    if (isNaN(userNumber)) {
+        alert("Hai sbagliato, scegli correttamente");
+        userNumber = 0;
+        //volendo si può iniziare a contare anche da qua
+    } else {
+        if (userNumber == randomGeneratedNumber) {
+            console.log("Ci sei riuscito ", userNumber ,"è uguale a ",randomGeneratedNumber);
+            console.log("il numero di tentativi è: ", counter);
+            check = true;
+    }   else if (userNumber > randomGeneratedNumber) {
+            alert("il numero generato è minore del numero scelto");
+            console.log("il numero generato è minore del numero scelto");
+            counter = counter + 1;
+    }   else {
+            alert("il numero generato è maggiore del numero scelto");
+            console.log("il numero generato è maggiore del numero scelto");
+            counter = counter + 1;
+    }
+  }
+}while(!check);
